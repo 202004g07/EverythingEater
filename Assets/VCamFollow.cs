e8f9e8831bfe0d53e485ec10fec602ed870eadee
Siki02 <56345423+Siki02@users.noreply.github.com>
2020-11-04 20:20:15 +0900

    使用していない部分の削除

--- Assets/VCamFollow.cs
+++ Assets/VCamFollow.cs
@@ -5,22 +5,11 @@ using UnityEngine;
 
 public class VCamFollow : MonoBehaviour
 {
-    CinemachineVirtualCamera VCam;
-
     [SerializeField]
     CinemachineTargetGroup TargetGroup;
     [SerializeField]
     float Radius = 4;
-    // Start is called before the first frame update
-    void Start()
-    {
-        VCam = GetComponent<CinemachineVirtualCamera>();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        // if (Player.Count() == 1) VCam.Follow = Player.BiggestPlayer;
-    }
+
     public void AddCinemaChineGroup(Transform target)
     {
         TargetGroup.AddMember(target, 1, Radius);
