135570106ff211c5a92844a98473f58d4e238194
Siki02 <56345423+Siki02@users.noreply.github.com>
2020-11-06 20:02:59 +0900

    げーむおーばーシーンひな形作成

---
+++ Assets/GameOverSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    Text FinishSizeText;
+    [SerializeField]
+    EatableObjSO Player;
+
+    void Start()
+    {
+        var integer = Mathf.Floor(Player.Size);
+        var AfterTheDecimalPoint = Player.Size - integer;
+        FinishSizeText.text = $"さいず:{integer}㍍ {AfterTheDecimalPoint:00}㌢";
+    }
+}
